025649094974ea51cb0bd5e866b41e47087185b1
Jakub <sirthaven@gmail.com>
2015-10-14 23:59:13 +0200

    tying to integrate deezer

---
+++ AngularJSWebApiEmpty2/Controllers/DeezerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularJSWebApiEmpty.Controllers
+{
+    public class DeezerController : ApiController
+    {
+
+        // GET: api/Deezer/5
+        public string Get(string error_reson, int? code)
+        {
+            return "dwed";
+        }
+    }
+}
